57ba639f755fba8035b716a4aa3ac88032121e4e
Shubhamsagar07 <shubhamsagarprasad@gmail.com>
2024-03-31 00:58:22 +0530

    UPDATE: added vision page

--- client/src/Pages/Vision.jsx
+++ client/src/Pages/Vision.jsx
@@ -2,7 +2,19 @@
 export default function Vision() {
     return (
         <div className="gradient-bg-welcome min-h-screen">
-        Vision
+         <div className=" border-t-2 border-b-2 border-l-2 border-r-2 rounded-2xl px-4 py-4 pt-4 pb-4">
+        <div className=" text-center text-3xl text-white py-5">Vision </div>
+       
+        <div className=" text-justify justify-end cursor-pointer font-semibold text-slate-50 px-64">At <b className=" hover:text-2xl ">[Eth-Funding]</b>, we believe in the power of collective action to drive positive change and innovation. Our platform serves as a dynamic hub where visionaries, creators, and changemakers from around the world come together to transform ideas into reality.</div>
+        <div className=" text-white px-64 py-6 text-2xl">Our Vision</div>
+        <div className=" px-72 py-2 text-justify font-semibold text-white">Our vision is to empower individuals and communities to bring their  dreams to life. We envision a world where anyone with a passion and a purpose can access the resources and support they need to make a difference.</div>
+        <div className=" px-64 py-2 text-white text-2xl">What Drives Us?</div>
+        <div className="py-4 px-72 text-white font-semibold justify-end cursor-pointer text-justify"> At this platform  [Eth-funding] lies a deep commitment to democratizing access to funding and opportunity. We believe that everyone  regardless of background or circumstance should be able to use this platform for their need and raise funds without any problem . By providing a platform that is inclusive, transparent, and supportive, we aim to level the playing field and amplify diverse voices and perspectives.</div>
+        <div className=" px-64 py-2 text-white text-2xl font-extrabold">Join Us in Building a Better Future!!</div>
+        <div className=" px-72 py-4 text-white font-semibold justify-end cursor-pointer text-justify">Whether you're an entrepreneur with a game-changing idea, an artist seeking to share your vision with the world, or a supporter looking to contribute to causes you care about, <b className=" text-2xl font-bold">[Eth-Funding]</b> welcomes you. Together, we can harness the power of crowdfunding to create positive change and shape a brighter tomorrow.</div>
+        <div className="px-64 text-white text-2xl font-extrabold">Start Your Journey Today</div>
+        <div className="px-72 py-2 text-white font-semibold justify-end cursor-pointer text-justify">Explore the projects and campaign on our platform, connect with inspiring peoples, and become part of a global community dedicated to turning dreams into reality. Join us in building a future where innovation knows no bounds and where every voice is heard. </div>
+        </div>
         </div>
     )
 }
